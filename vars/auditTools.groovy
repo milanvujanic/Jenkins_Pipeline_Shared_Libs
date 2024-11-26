8b0e36c540f79a96cb4c8a64c91d39012ee285f4
milanvujanic <milanvujanic79@gmial.com>
2024-11-26 08:46:19 +0100

    small changes

--- vars/auditTools.groovy
+++ vars/auditTools.groovy
@@ -3,6 +3,8 @@ def call() {
       sh '''
         git version
         java -version
+        export MAVEN_HOME=/opt/apache-maven-3.9.8
+        export PATH=${PATH}:${JAVA_HOME}/bin:${MAVEN_HOME}/bin
         mvn -version
       '''
   }
--- vars/auditTools2.groovy
+++ vars/auditTools2.groovy
@@ -4,6 +4,8 @@ def call(Map config) {
       sh '''
         git version
         java -version
+        export MAVEN_HOME=/opt/apache-maven-3.9.8
+        export PATH=${PATH}:${JAVA_HOME}/bin:${MAVEN_HOME}/bin
         mvn -version
       '''
     }
